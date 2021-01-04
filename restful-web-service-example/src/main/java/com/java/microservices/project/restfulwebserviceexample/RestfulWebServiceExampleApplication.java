35b9f6d3a2382b442aeaa34e5c05cd4da640e94c
stephen.mcdonagh <stephen.mcdonagh@bearingpoint.com>
2021-01-04 17:31:21 +0000

    init commit

---
+++ restful-web-service-example/src/main/java/com/java/microservices/project/restfulwebserviceexample/RestfulWebServiceExampleApplication.java
@@ -0,0 +1,13 @@
+package com.java.microservices.project.restfulwebserviceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceExampleApplication.class, args);
+	}
+
+}
---
+++ restful-web-service-example/src/test/java/com/java/microservices/project/restfulwebserviceexample/RestfulWebServiceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.microservices.project.restfulwebserviceexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
